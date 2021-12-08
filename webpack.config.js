ba075eeb9dfd22ea0a8eda83205884bd51aded89
Kevin Gottfried <kgott541@gmail.com>
2021-12-07 21:12:19 -0800

    initializing project

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  devServer: {
+    'static': {
+      directory: './dist'
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>Restaurant Roulette</title></head><body><div id=\"app\"></div></body></html>',
+      filename: 'index.html',
+    }),
+  ]
+};
+
+module.exports = config;
