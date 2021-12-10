614ca84f82eb5588e962869ffc491022fe043ade
Kevin Gottfried <kgott541@gmail.com>
2021-12-09 21:49:40 -0800

    added google login authenticatoin and sign out

--- src/App.js
+++ src/App.js
@@ -4,26 +4,25 @@ import { hot } from 'react-hot-loader/root';
 import Button from '@material-ui/core/Button';
 import Stack from '@mui/material/Stack';
 import { GoogleLogin } from 'react-google-login';
+import { GoogleLogout } from 'react-google-login';
+import GoogleLoginComponent from "./googlebutton.component";
 
 class App extends React.Component {
   render() {
     const { name } = this.props;
-    const responseGoogle = (response) => {
-      console.log(response);
-    }
+    
     return (
-      <>
-      <GoogleLogin
-        clientId="717956367850-cg8brdm11t24qeqs5jhkiqgn7kbglncd.apps.googleusercontent.com"
-        buttonText="Login with Google"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy="single_host_origin"
-      />    
+      <div>
+      <GoogleLoginComponent />
+      {/* <GoogleLogout
+      clientId="717956367850-cg8brdm11t24qeqs5jhkiqgn7kbglncd.apps.googleusercontent.com"
+      buttonText="Logout"
+      onLogoutSuccess={logout}
+      /> */}
       <h1>Welcome to Restaurant Roulette</h1>
       <h2>You're home for choosing a restaurant</h2>
       <Button variant="contained">Get Started</Button>
-      </>
+      </div>
     );
   }
 }
